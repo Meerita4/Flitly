34686ec79501074ba6e7f8196b1e3323fb85d7e4
Alexthfc10 <116588914+meerita4@users.noreply.github.com>
2026-05-14 18:40:29 +0200

    descargas añadido

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,7 @@
 
 export const SITE_TITLE = 'Astro Blog';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+
+// Store URLs for direct app downloads.
+export const APP_STORE_URL = 'https://apps.apple.com/es/app/flitly/id6766552831';
+export const PLAY_STORE_URL = 'https://play.google.com/store/apps/details?id=com.flitly.flitly';
